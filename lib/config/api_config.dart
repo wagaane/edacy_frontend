fc507807b50c1e2414d74536b1b42f38097d5499
wagaane <ablayefaye9727@gmail.com>
2025-05-03 07:08:31 +0000

    first commit

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,8 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiConfig{
-  // static String API_URL = "http://localhost:8000/api/v1/";
-  static String API_URL = "http://192.168.100.38:8000/api/v1/";
+  static String API_URL = "http://localhost:8000/api/v1/";
+  // static String API_URL = "http://192.168.100.38:8000/api/v1/";
   // static String API_URL = "http://192.168.1.147:8000/api/v1/";
 
   static getToken() async{
